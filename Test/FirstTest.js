5504032cd6c252274a7005cfc5aefc986b7ca3ad
Nojus Vilpišauskas <nojusvilpisauskas@gmail.com>
2024-09-25 16:34:24 +0300

    Test using mocha

---
+++ Test/FirstTest.js
@@ -0,0 +1,34 @@
+import { Builder, By, Key } from "selenium-webdriver";
+import { should } from "chai"; // Import should from chai (named export)
+
+// Initialize should assertions
+should();
+
+describe("add todo  tests", function () {
+  it("successfully adds todo to aplication", async function () {
+    // launch browser
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    // go to website
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    // add a to do
+    await driver
+      .findElement(By.id("sampletodotext"))
+      .sendKeys("Learn Selenium", Key.RETURN);
+
+    // get element from list and store it
+    let todoText = await driver
+      .findElement(By.xpath("//li[last()]"))
+      .getText()
+      .then(function (value) {
+        return value;
+      });
+
+    // assert with chai
+    todoText.should.equal("Learn Selenium");
+
+    // close browser
+    await driver.quit();
+  });
+});
